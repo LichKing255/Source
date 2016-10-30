eb089e62792dac21afef8ea5647672dd6a320ad6
mihangram <maryamrezaymihan@gmail.com>
2016-10-30 16:56:14 +0330

    Create BuildVars.java

---
+++ TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
@@ -0,0 +1,38 @@
+package com.mihan.mihangram;
+
+public class BuildVars
+{
+  public static String APP_HASH;
+  public static int APP_ID;
+  public static String BING_SEARCH_KEY;
+  public static int BUILD_VERSION;
+  public static String BUILD_VERSION_STRING;
+  public static boolean DEBUG_VERSION = false;
+  public static String FOURSQUARE_API_ID = "20150326";
+  public static String FOURSQUARE_API_KEY;
+  public static String FOURSQUARE_API_VERSION = "20150326";
+  public static String GCM_SENDER_ID;
+  public static String HOCKEY_APP_HASH;
+  public static String HOCKEY_APP_HASH_DEBUG;
+  public static String SEND_LOGS_EMAIL;
+  
+  static
+  {
+    BUILD_VERSION = 821;
+    BUILD_VERSION_STRING = "3.10.1";
+    APP_ID = 6;
+    APP_HASH = "7628a3e99d2a866b1b946b59b4eafe6e";
+    HOCKEY_APP_HASH = "7628a3e99d2a866b1b946b59b4eafe6e";
+    HOCKEY_APP_HASH_DEBUG = "760348033673";
+    SEND_LOGS_EMAIL = "maryamrezaymihan@gmail.com";
+    BING_SEARCH_KEY = "";
+    FOURSQUARE_API_KEY = "";
+    FOURSQUARE_API_ID = "";
+  }
+}
+
+
+/* Location:              \org\telegram\messenger\BuildVars.class
+ * Java compiler version: 6 (50.0)
+ * JD-Core Version:       0.7.1
+ */
