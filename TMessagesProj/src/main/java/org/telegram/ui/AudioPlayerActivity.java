0595d3105faccec9f18fba5520412da7c97e1224
Supergram <maryamrezaymihan@gmail.com>
2016-12-01 22:56:03 +0330

    new

--- TMessagesProj/src/main/java/org/telegram/ui/AudioPlayerActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/AudioPlayerActivity.java
@@ -565,6 +565,6 @@ public class AudioPlayerActivity
 
 
 /* //Location:              C:\Users\Downloads\Compressed\dex2jar-2.0\classes-dex2jar.jar!\org\telegram\ui\AudioPlayerActivity.class
- * Java compiler version: 6 (50.0)
- * JD-Core Version:       0.7.1
+ * //Java compiler version: 6 (50.0)
+ * //JD-Core Version:       0.7.1
  */
